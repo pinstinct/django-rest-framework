11023b0350b5f2d4f8ab012e3379a3a2122bb1c5
LimHM <gusals3407@gmail.com>
2017-03-13 20:16:21 +0900

    Add snippets app

--- django_app/config/settings.py
+++ django_app/config/settings.py
@@ -31,6 +31,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+
+    'snippets',
 ]
 
 MIDDLEWARE = [
