10c04835b6ca8db032be3e9f26a9a2e1f2e585bf
LimHM <gusals3407@gmail.com>
2017-03-13 22:41:37 +0900

    Add required permission

--- django_app/snippets/views.py
+++ django_app/snippets/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import generics
+from rest_framework import permissions
 
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
@@ -18,6 +19,7 @@ class UserDetail(generics.RetrieveAPIView):
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -26,3 +28,4 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
